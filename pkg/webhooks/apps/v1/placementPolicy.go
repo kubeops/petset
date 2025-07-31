0008f307eac261970dac13e64c8ae3f53c84baf1
souravbiswassanto <saurov@appscode.com>
2025-07-31 10:03:34 +0600

    change spec -> rules
    Signed-off-by: souravbiswassanto <saurov@appscode.com>

--- pkg/webhooks/apps/v1/placementPolicy.go
+++ pkg/webhooks/apps/v1/placementPolicy.go
@@ -142,18 +142,18 @@ func (w *PlacementPolicyCustomWebhook) validateUpdatePlacementPolicy(_ context.C
 }
 
 // flattenReplicas takes the cluster spec and returns a single slice of all replica indices.
-func flattenReplicas(spec []api.DistributionRule) []int32 {
+func flattenReplicas(rules []api.DistributionRule) []int32 {
 	var allReplicas []int32
-	for _, cluster := range spec {
+	for _, cluster := range rules {
 		allReplicas = append(allReplicas, cluster.Replicas...)
 	}
 	return allReplicas
 }
 
 // buildReplicaClusterMap creates a map of replica index to its assigned cluster name.
-func buildReplicaClusterMap(spec []api.DistributionRule) map[int32]string {
+func buildReplicaClusterMap(rules []api.DistributionRule) map[int32]string {
 	replicaMap := make(map[int32]string)
-	for _, cluster := range spec {
+	for _, cluster := range rules {
 		for _, replica := range cluster.Replicas {
 			replicaMap[replica] = cluster.ClusterName
 		}
